f97e1e4ad880f78781da4f861bcba8b1cd2b972b
ccristil <130681546+ccristil@users.noreply.github.com>
2024-03-20 13:10:07 -0600

    final update for pagination

--- obj/Debug/net8.0/Mission11_Cruz.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission11_Cruz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Cruz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082a008d77333d54d1e6395986ffebfa555b89c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30026d7d862ca7677fe8e9615dd4baf2f944f5e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Cruz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Cruz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
