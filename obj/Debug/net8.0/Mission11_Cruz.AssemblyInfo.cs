082a008d77333d54d1e6395986ffebfa555b89c9
ccristil <130681546+ccristil@users.noreply.github.com>
2024-03-20 11:27:05 -0600

    assembly info

--- obj/Debug/net8.0/Mission11_Cruz.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission11_Cruz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Cruz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda6621ab72618fd835c9f3e28c54cc920d83ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Cruz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Cruz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
